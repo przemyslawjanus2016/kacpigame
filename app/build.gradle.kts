cd0632bb111fd5f1ca1a91000afe098c762e39b2
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-13 20:25:11 +0200

    Prepare v0.5.0 for Google Play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.kacperikapi.mathadventure"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.4.1"
+        versionCode = 18
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
