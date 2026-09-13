2f4a6b4af4ad1cc3b2409b2598618193162c5402
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-13 18:01:10 +0200

    Enable production level locks

---
+++ app/src/main/java/pl/kacperikapi/mathadventure/data/DevOptions.kt
@@ -0,0 +1,6 @@
+package pl.kacperikapi.mathadventure.data
+
+/** Production content locks. Enable only in a dedicated internal preview build. */
+object DevOptions {
+    const val UNLOCK_ALL_CONTENT: Boolean = false
+}
