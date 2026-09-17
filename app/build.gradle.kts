41cd427da5104427572fe34b9e45e32657d5d29e
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-17 14:39:01 +0200

    Bump test build to v0.5.5 (23)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.janusdigital.kacperikapi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.5.4"
+        versionCode = 23
+        versionName = "0.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
