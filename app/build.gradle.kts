846e9b87c9c793e931f09f9700eb3f0b7171c7cd
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-17 16:24:41 +0200

    Bump test build to v0.5.6 (24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.janusdigital.kacperikapi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.5.5"
+        versionCode = 24
+        versionName = "0.5.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
@@ -87,16 +87,23 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.12.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
-    implementation("com.android.billingclient:billing-ktx:9.1.0")
+    implementation("com.android.billingclient:billing-ktx:8.3.0")
+    implementation("com.google.android.gms:play-services-ads:25.0.0")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-
-    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.runtime:runtime-saveable")
+    implementation("androidx.compose.ui:ui-text-google-fonts")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
