16af2b156aea65ddc47565bd8c98bd9c8c489434
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-18 09:57:14 +0200

    Release v0.5.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.janusdigital.kacperikapi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "0.5.8"
+        versionCode = 27
+        versionName = "0.5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
