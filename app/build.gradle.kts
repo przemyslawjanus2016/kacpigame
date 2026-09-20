e6fc42f09f9eae614287c32ccd51441a93e06217
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-20 21:36:49 +0200

    Bump v0.5.10 test build code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "pl.janusdigital.kacperikapi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
+        versionCode = 29
         versionName = "0.5.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
