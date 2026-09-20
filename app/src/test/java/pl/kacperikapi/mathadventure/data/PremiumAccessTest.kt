e45ceb06c71c9b266e2b20a5f1fc6fb8e2cb2728
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-20 20:43:04 +0200

    Make premium tests respect closed-test unlock mode

--- app/src/test/java/pl/kacperikapi/mathadventure/data/PremiumAccessTest.kt
+++ app/src/test/java/pl/kacperikapi/mathadventure/data/PremiumAccessTest.kt
@@ -14,7 +14,11 @@ class PremiumAccessTest {
     @Test
     fun laterWorldsNeedPremiumAndProgress() {
         assertTrue(PremiumAccess.requiresPremium(2))
-        assertFalse(PremiumAccess.canOpenWorld(2, progressUnlocked = true, premiumUnlocked = false))
+        if (DevOptions.UNLOCK_ALL_CONTENT) {
+            assertTrue(PremiumAccess.canOpenWorld(2, progressUnlocked = true, premiumUnlocked = false))
+        } else {
+            assertFalse(PremiumAccess.canOpenWorld(2, progressUnlocked = true, premiumUnlocked = false))
+        }
         assertFalse(PremiumAccess.canOpenWorld(2, progressUnlocked = false, premiumUnlocked = true))
         assertTrue(PremiumAccess.canOpenWorld(2, progressUnlocked = true, premiumUnlocked = true))
     }
@@ -22,7 +26,11 @@ class PremiumAccessTest {
     @Test
     fun paywallOnlyAppliesToPremiumWorldsWithoutEntitlement() {
         assertFalse(PremiumAccess.shouldShowPaywall(1, premiumUnlocked = false))
-        assertTrue(PremiumAccess.shouldShowPaywall(2, premiumUnlocked = false))
+        if (DevOptions.UNLOCK_ALL_CONTENT) {
+            assertFalse(PremiumAccess.shouldShowPaywall(2, premiumUnlocked = false))
+        } else {
+            assertTrue(PremiumAccess.shouldShowPaywall(2, premiumUnlocked = false))
+        }
         assertFalse(PremiumAccess.shouldShowPaywall(2, premiumUnlocked = true))
     }
 }
