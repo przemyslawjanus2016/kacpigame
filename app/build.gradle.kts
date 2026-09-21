379e27f585fc009ef41c549b43a40d351be6f2e0
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-21 20:30:25 +0200

    Bump v0.5.10 profile-fix build code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "pl.janusdigital.kacperikapi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 31
+        versionCode = 32
         versionName = "0.5.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
