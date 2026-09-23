df216ef521e0868e9be66f5fc1b6ff15c738fa5d
przemyslawjanus2016 <przemyslawjanus2016@gmail.com>
2026-09-23 19:03:50 +0200

    Bump app to v0.6.2 build 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.janusdigital.kacperikapi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
-        versionName = "0.6.1"
+        versionCode = 36
+        versionName = "0.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
